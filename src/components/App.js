44d22cb38c16d93f97a4693f048fd0733d049786
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-02-04 03:43:13 +0200

    added notice categories

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import { SharedLayout } from './sharedLayout/SharedLayout';
-
+import { NoticeCategoryList } from './noticesCategoryList/NoticeCategoryList';
 const RegisterPage = lazy(() => import('../pages/registerPage/RegisterPage'));
 const LoginPage = lazy(() => import('../pages/loginPage/LoginPage'));
 const NewsPage = lazy(() => import('../pages/newsPage/NewsPage'));
@@ -17,8 +17,15 @@ const App = () => {
         <Route path='/' element={<SharedLayout />}>
           <Route index element={<NewsPage />}/>
           <Route path='/news' element={<NewsPage />}/>
-          <Route path='/notices' element={<NoticesPage />}/>
-          <Route path='/notices/:categoryName' element={<NewsPage />}/>
+          <Route path='/notices' element={<NoticesPage />}>
+          <Route path='/notices/sell' element={<NoticeCategoryList />}/>
+          <Route path='/notices/lost-found' element={<NoticeCategoryList />}/>
+          <Route path='/notices/for-free' element={<NoticeCategoryList />}/>
+          </Route>
+          <Route path='/notices/:categoryName' element={<NoticesPage />}>
+          <Route path='/notices/favorite' element={<NoticeCategoryList />}/>
+          <Route path='/notices/own' element={<NoticeCategoryList />}/>
+          </Route>
           <Route path='/friends' element={<OurFriendsPage />}/>
           <Route path='/register' element={<RegisterPage />}/>
           <Route path='/login' element={<LoginPage />}/>
