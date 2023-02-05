bfe3184ef0f91b01bbb4183ec9d96719e33bb622
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-02-05 11:43:40 +0200

    added logout

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,14 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
-
+import { logOut } from './operations';
 const authInitialState = {
   user: { name: null, email: null },
   token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+  error: null,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState: authInitialState,
-  extraReducers: {},
+  extraReducers: {
+    [logOut.fulfilled](state) {
+      state.user = {name: null, email: null};
+      state.token = null;
+      state.isLoggedIn = false;
+      state.error = null;
+  },
+  },
 });
 
 export const authReducer = authSlice.reducer;
