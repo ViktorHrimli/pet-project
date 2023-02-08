7f0043de020d7db155b872c828effedd47e72825
Victoria Myzynyuk <mezochka93@gmail.com>
2023-02-08 21:48:16 +0200

    burger close on link click

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,11 +1,15 @@
 import { NAV_LIST } from './constants';
 import { StyledNavLink, NavigationWrapper } from 'components/nav/Nav.styled';
 
-export const Nav = () => {
+export const Nav = ({ setOpen }) => {
   return (
     <NavigationWrapper>
       {NAV_LIST.map(item => (
-        <StyledNavLink to={`/${item.link}`} key={item.label}>
+        <StyledNavLink
+          to={`/${item.link}`}
+          key={item.label}
+          onClick={() => setOpen(false)}
+        >
           {item.label}
         </StyledNavLink>
       ))}
--- src/components/userNav/UserNav.js
+++ src/components/userNav/UserNav.js
@@ -1,10 +1,19 @@
 import { StyledNavLink, AvatarIconEl } from 'components/userNav/UserNav.styled';
+import { useBreakpoint } from 'hooks/useBreakpoint';
 
 import avatarIcon from 'images/svg/default-avatar.png';
 
-export const UserNav = () => {
+export const UserNav = ({ setOpen }) => {
+  const { isMobileScreen } = useBreakpoint();
   return (
-    <StyledNavLink to="/user">
+    <StyledNavLink
+      to="/user"
+      onClick={() => {
+        if (isMobileScreen) {
+          setOpen(false);
+        }
+      }}
+    >
       <AvatarIconEl src={avatarIcon} alt="avatar" />
       Account
     </StyledNavLink>
