fb3426c32254ba10bfec7eb99a21484d3a6f03b4
Victoria Myzynyuk <mezochka93@gmail.com>
2023-02-09 19:20:07 +0200

    fix-three

--- src/components/userData/UserItem.js
+++ src/components/userData/UserItem.js
@@ -5,88 +5,89 @@ import { toggleIsDisablet } from 'redux/user/slice';
 import { ReactComponent as ApproveIcon } from '../../images/svg/stroke.svg';
 import { ReactComponent as EditIcon } from '../../images/svg/ci_edit.svg';
 
-import { EditInput, InfoItem, Form, Userlabel } from 'components/userData/styles/UserDataItem.styles';
+import {
+  EditInput,
+  InfoItem,
+  Form,
+  Userlabel,
+} from 'components/userData/styles/UserDataItem.styles';
 
 export const UserItem = ({
-	data,
-	errors,
-	type = 'text',
-	register,
-	field,
-	text,
-	setValue,
-	onSubmit,
-	pattern: { pattern, patternMessage },
+  data,
+  errors,
+  type = 'text',
+  register,
+  field,
+  text,
+  setValue,
+  onSubmit,
+  pattern: { pattern, patternMessage },
 }) => {
-	const [edited, setEdited] = useState(false);
+  const [edited, setEdited] = useState(false);
 
-	const isLoadingUpdate = useSelector(getLoadingUpdate);
-	const isDisabled = useSelector(getDisabledFields);
-	const dispatch = useDispatch();
+  const isLoadingUpdate = useSelector(getLoadingUpdate);
+  const isDisabled = useSelector(getDisabledFields);
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		if (isLoadingUpdate) {
-			setEdited(false);
-		}
-	}, [isLoadingUpdate]);
+  useEffect(() => {
+    if (isLoadingUpdate) {
+      setEdited(false);
+    }
+  }, [isLoadingUpdate]);
 
-	const checkDisabled = () => {
-		if (!isDisabled) {
-			dispatch(toggleIsDisablet());
-			setEdited(true);
-		}
-	};
+  const checkDisabled = () => {
+    if (!isDisabled) {
+      dispatch(toggleIsDisablet());
+      setEdited(true);
+    }
+  };
 
-	return (
-		<Form onSubmit={onSubmit}>
-			<Userlabel htmlFor={data} >
-				<InfoItem>{text}:</InfoItem>
-				{edited ? (
-					<>
-						<EditInput
-							{...register(field, {
-								pattern: {
-									value: pattern,
-									message: patternMessage,
-								},
-							})}
+  return (
+    <Form onSubmit={onSubmit}>
+      <Userlabel htmlFor={data}>
+        <InfoItem>{text}:</InfoItem>
+        {edited ? (
+          <>
+            <EditInput
+              {...register(field, {
+                pattern: {
+                  value: pattern,
+                  message: patternMessage,
+                },
+              })}
+              type={type}
+              name={field}
+              id={data}
+              placeholder={`Your ${field}`}
+            />
 
-							type={type}
-							name={field}
-							id={data}
-							placeholder={`Your ${field}`}
-						/>
+            {errors[field] && <div>{errors[field]?.message}</div>}
+          </>
+        ) : (
+          <p>{data === '' ? `Unknown` : data}</p>
+        )}
 
-						{errors[field] && (
-							<div>
-								{errors[field]?.message}
-							</div>
-						)}
-					</>
-				) : (
-					<p>{data === '' ? `Unknown` : data}</p>
-				)}
+        {edited ? (
+          <button type="submit">
+            <ApproveIcon fill="currentColor" width="20px" height="20px" />
+          </button>
+        ) : (
+          <div
+            onClick={() => {
+              setValue(field, data);
+              checkDisabled();
+            }}
+          >
+            <EditIcon
+              fill={isDisabled ? 'rgba(17, 17, 17, 0.6)' : 'currentColor'}
+              width="20px"
+              height="20px"
+            />
+          </div>
+        )}
+      </Userlabel>
+    </Form>
 
-				{edited ? (
-					<button type="submit">
-						<ApproveIcon fill="currentColor" width="20px" height="20px" />
-					</button>
-				) : (
-					<div
-
-						onClick={() => {
-							setValue(field, data);
-							checkDisabled();
-						}}
-					>
-						<EditIcon
-							fill={isDisabled ? 'rgba(17, 17, 17, 0.6)' : 'currentColor'}
-							width="20px"
-							height="20px"
-						/>
-					</div>
-				)}
-			</Userlabel>
-		</Form>
-	);
+    // <hey></hey>
+  );
 };
