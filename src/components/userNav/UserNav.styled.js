a687876ae3129300b28e082d4993eec7212f0636
Victoria Myzynyuk <mezochka93@gmail.com>
2023-02-09 18:50:02 +0200

    fix

--- src/components/userNav/UserNav.styled.js
+++ src/components/userNav/UserNav.styled.js
@@ -34,6 +34,7 @@ export const StyledNavLink = styled(NavLink)`
     font-size: 16px;
     margin-right: 55px;
     margin-bottom: 0;
+    /* padding: 10px; */
   }
 
   @media screen and (min-width: 1280px) {
