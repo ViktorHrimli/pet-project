47258132a6fc51073e44851474720317d64c6184
Victoria Myzynyuk <mezochka93@gmail.com>
2023-02-11 20:09:34 +0200

    fix crush

--- src/redux/user/operations.js
+++ src/redux/user/operations.js
@@ -39,28 +39,6 @@ export const getUserData = createAsyncThunk(
   }
 );
 
-export const addPet = createAsyncThunk(
-  'pets/addPet',
-  async ({ name, breed, comments, date, photo, token }, thunkAPI) => {
-    const formData = new FormData();
-
-    formData.append('name', name);
-    formData.append('breed', breed);
-    formData.append('comments', comments);
-    formData.append('date', date);
-    formData.append('photo', photo);
-
-    try {
-      const { data } = await axios.post('/pets/add', formData, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      return data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
-    }
-  }
-);
-
 // export const getUserData = createAsyncThunk(
 // 	'user/getUserData',
 // 	async (_, { rejectWithValue }) => {
