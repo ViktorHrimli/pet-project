3ee315f20f609227725fe2a5b8505bf823d9cdca
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-02-14 04:30:44 +0200

    fix-email

--- src/components/modalNotice/ModalNotice.js
+++ src/components/modalNotice/ModalNotice.js
@@ -103,7 +103,7 @@ export const ModalNotice = ({data, onClose}) => {
             </InfoItem>
             <InfoItem>
               <LableNotice>Email:</LableNotice>
-              {email && <a href={email}>
+              {email && <a href={`email:${email}`}>
                 <DateModalNotice>{email}</DateModalNotice>
               </a>}
             </InfoItem>
