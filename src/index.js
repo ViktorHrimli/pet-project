2975e1fb785f30fb58e3e88a9443f7e8fe805199
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-02-21 00:54:32 +0200

    main

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="team-project-react-petly">
           <ThemeProvider theme={theme}>
             <Global styles={GlobalStyles} />
             <App />
